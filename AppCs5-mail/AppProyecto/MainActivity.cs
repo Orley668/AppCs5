1cda42ed4f7d570eab3572ff7d02ca4e7bff33b7
Orley668 <114945675+Orley668@users.noreply.github.com>
2023-04-07 21:30:21 -0500

    cambios

---
+++ AppCs5-mail/AppProyecto/MainActivity.cs
@@ -0,0 +1,76 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace AppProyecto
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        EditText txtUsuario;
+        EditText txtClave;
+        Button btnIngresar;
+        Button btnRegistrar;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            txtUsuario = FindViewById<EditText>(Resource.Id.txtUser);
+            txtClave = FindViewById<EditText>(Resource.Id.txtPassword);
+            btnIngresar = FindViewById<Button>(Resource.Id.btnLogin);
+            btnRegistrar = FindViewById<Button>(Resource.Id.btnSingIn);
+
+            btnIngresar.Click += BtnIngresar_Click;
+            btnRegistrar.Click += BtnRegistrar_Click;
+        }
+
+        private void BtnRegistrar_Click(object sender, System.EventArgs e)
+        {
+            Intent i = new Intent(this, typeof(RegistrarUsuario));
+            StartActivity(i);
+        }
+
+        private void BtnIngresar_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                Login resultado = null;
+                if (!string.IsNullOrEmpty(txtUsuario.Text.Trim()) && !string.IsNullOrEmpty(txtClave.Text.Trim()))
+                {
+                    resultado = new Auxiliar().SeleccionarUno(txtUsuario.Text.Trim(), txtClave.Text.Trim());
+                    if (resultado != null)
+                    {
+                        txtUsuario.Text = resultado.User.ToString();
+                        Toast.MakeText(this, "Login realizado con exito", ToastLength.Short).Show();
+                        var menu = new Intent(this, typeof(Menu));
+                        menu.PutExtra("usuario", FindViewById<EditText>(Resource.Id.txtUser).Text);
+                        StartActivity(menu);
+                        Finish();
+                    }
+                    else
+                    {
+                        Toast.MakeText(this, "Nombre de usuario y/o clave inválida(s)", ToastLength.Long).Show();
+                    }
+                }
+                else
+                {
+                    Toast.MakeText(this, "Nombre de usuario y/o clave son vacios", ToastLength.Long).Show();
+                }
+
+            }
+            catch
+            {
+
+            }
+        }
+
+
+    }
+}
---
+++ AppCs5-mail/AppProyecto/obj/Debug/120/android/src/crc645c43a17a0674987c/Create_Ticket.java
@@ -0,0 +1,48 @@
+package crc645c43a17a0674987c;
+
+
+public class Create_Ticket
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("AppProyecto.Create_Ticket, AppProyecto", Create_Ticket.class, __md_methods);
+	}
+
+
+	public Create_Ticket ()
+	{
+		super ();
+		if (getClass () == Create_Ticket.class) {
+			mono.android.TypeManager.Activate ("AppProyecto.Create_Ticket, AppProyecto", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
